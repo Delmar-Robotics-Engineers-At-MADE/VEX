20f0686cf52ce0b010aacfb7f2bf8699e4abacf6
Matt Snyder <slylock2@mac.com>
2020-10-09 12:41:08 -0500

    robot relative holonomic

---
+++ 2020-21/38077-B-MS/Clawbot-Holonomic-2020-10-09T17-31-42/include/robot-config.h
@@ -0,0 +1,17 @@
+using namespace vex;
+
+extern brain Brain;
+
+// VEXcode devices
+extern motor back_right_motor;
+extern motor back_left_motor;
+extern motor front_right_motor;
+extern motor front_left_motor;
+extern controller Controller1;
+
+/**
+ * Used to initialize code/tasks/devices added using tools in VEXcode Pro.
+ * 
+ * This should be called at the start of your int main function.
+ */
+void  vexcodeInit( void );
